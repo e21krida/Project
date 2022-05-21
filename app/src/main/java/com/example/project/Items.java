e1723182e4a8c086c1e200c23336adc200d01651
e21krida <e21krida@student.his.se>
2022-05-21 17:22:41 +0200

    Genererade getters samt setters inom Items.java

--- app/src/main/java/com/example/project/Items.java
+++ app/src/main/java/com/example/project/Items.java
@@ -1,4 +1,25 @@
 package com.example.project;
 
 public class Items {
+    private String login;
+    private int ID;
+    public Items(String login, int ID) {
+        this.login = login;
+        this.ID = ID;
+    }
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
 }
