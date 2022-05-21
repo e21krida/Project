0b72d11948578289b31c7f891225e74c1bd066da
e21krida <e21krida@student.his.se>
2022-05-21 17:16:51 +0200

    Skapade filerna Items.java, MyAdapter.java samt MyViewHolder.java, genererade onCreateViewHolder, onBindViewHolder samt getItemCount inom MyAdapter.java. Skapde även en recyclerview i min activity_main.xml samt tillhörande recyclerview_items.xml som jag länkade in i MainActivity.java

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView RecyclerView;
+    private MyAdapter adapter;
+    private ArrayList<Items> listOfItems;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView = findViewById(R.id.recycler_view);
+        RecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new MyAdapter(listOfItems);
+        RecyclerView.setAdapter(adapter);
+
     }
 }
