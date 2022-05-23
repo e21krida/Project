c1619151c5c644b7a8dee15c71c8da22904570fb
e21krida <e21krida@student.his.se>
2022-05-23 13:54:44 +0200

    Skapade JSON-datan, anpassade Items.java samt recyclerview_items.xml efter den valda datan.

--- app/src/main/java/com/example/project/Items.java
+++ app/src/main/java/com/example/project/Items.java
@@ -1,25 +1,47 @@
 package com.example.project;
 
 public class Items {
-    private String login;
-    private int ID;
-    public Items(String login, int ID) {
-        this.login = login;
+    private String ID;
+    private String name;
+    private String location;
+    private String category;
+
+    public Items(String ID, String name, String location, String category) {
         this.ID = ID;
-    }
-    public String getLogin() {
-        return login;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
+        this.name = name;
+        this.location = location;
+        this.category = category;
     }
 
-    public int getID() {
+    public String getID() {
         return ID;
     }
 
-    public void setID(int ID) {
+    public void setID(String ID) {
         this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 }
